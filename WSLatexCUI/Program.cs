8c78284300a8d837468d3faf4e9635da4ed2688a
Masaharu Kato <fudai.mk@gmail.com>
2022-11-18 01:53:50 +0900

    Created basic Latex command runner

---
+++ WSLatexCUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WSLatexCUI // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunLatex.GenerateSVGFromTex(args[0]);
+        }
+    }
+}
---
+++ WSLatexCUI/RunLatex.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSLatexCUI
+{
+    internal class RunLatex
+    {
+        static string SaveTexToFile(string content)
+        {
+            string filename = "temp"; // TODO: Implement
+            File.WriteAllText($"{filename}.tex", content);
+            return "temp";
+        }
+        
+        static void GenerateDVIFromTex(string filename)
+        {
+            RunCommand.RunOnWSL(
+                "pdflatex",
+                $"-output-format dvi -shell-escape -interaction=batchmode {filename}.tex");
+        }
+
+        static void GenerateSVGFromDVI(string filename)
+        {
+            RunCommand.RunOnWSL(
+                "dvisvgm", 
+                $"--no-fonts -o {filename}.svg {filename}.dvi");
+        }
+
+        static public void GenerateSVGFromTex(string filename)
+        {
+            GenerateDVIFromTex(filename);
+            GenerateSVGFromDVI(filename);
+        }
+
+        static public void GenerateSVGFromTexContent(string content)
+        {
+            string filename = SaveTexToFile(content);
+            GenerateSVGFromTex(filename);
+        }
+    }
+}
